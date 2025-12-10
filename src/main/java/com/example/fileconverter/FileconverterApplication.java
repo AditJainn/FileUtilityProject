87642faaffc2b613440f7ebb96ea37320c125853
AditJainn <77595232+AditJainn@users.noreply.github.com>
2025-12-09 23:56:03 -0500

    Update FileconverterApplication.java

--- src/main/java/com/example/fileconverter/FileconverterApplication.java
+++ src/main/java/com/example/fileconverter/FileconverterApplication.java
@@ -2,8 +2,6 @@ package com.example.fileconverter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.apache.pdfbox.pdmodel.*;
-import org.apache.pdfbox.pdmodel.graphics.image.PDImageXObject;
 
 
 @SpringBootApplication
